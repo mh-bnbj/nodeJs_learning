bf56cf0be72357ff75a1bb55318f1aa6c37cf08e
mh-bnbj <www.mohamadhasan65@gmail.com>
2023-06-20 17:04:38 +0330

    e5.2 get data and async foreach
    get data : get all data form all file in folder input and save on data object
    
    async foreach : because readCSV function is async and we want await
    and normal foreach cant be used in aync ways so we use another method for asyncForEach

--- s3_nodejs-tools/e1_sample-nodejs-merger/index.js
+++ s3_nodejs-tools/e1_sample-nodejs-merger/index.js
@@ -1,8 +1,31 @@
-const { readCSV, readDirectory } = require('./src/utils/fileHandler')
+const {
+    readCSV,
+    readDirectory,
+    readSourcesNames,
+} = require('./src/utils/fileHandler')
+
+const { asyncForEach } = require('./src/utils/asyncForEach')
 
 // 1. Read CSV F1iles
 const readFiles = async () => {
-    const data = readDirectory('./input')
+    const fileNames = readDirectory('./input')
+    const sources = readSourcesNames(fileNames, 'catalog')
+    const data = []
+    await asyncForEach(sources, async (sourceName) => {
+        data[sourceName] = []
+
+        data[sourceName].catalog = await readCSV(
+            `./input/catalog${sourceName}.csv`
+        )
+
+        data[sourceName].suppliers = await readCSV(
+            `./input/suppliers${sourceName}.csv`
+        )
+
+        data[sourceName].barcodes = await readCSV(
+            `./input/barcodes${sourceName}.csv`
+        )
+    })
     console.log('data', data)
 }
 
--- s3_nodejs-tools/e1_sample-nodejs-merger/src/utils/fileHandler.js
+++ s3_nodejs-tools/e1_sample-nodejs-merger/src/utils/fileHandler.js
@@ -17,7 +17,21 @@ const readDirectory = (folderPath) => {
     return fs.readdirSync(folderPath)
 }
 
+const readSourcesNames = (fileNames, startWith) => {
+    const wantedNames = fileNames.filter((fileName) => {
+        return fileName.startsWith(startWith)
+    })
+
+    const sources = wantedNames.map((wantedName) => {
+        // using rejex to delete catalog and .csv from catalogA.csv
+        return wantedName.replace(/catalog|.csv/g, '')
+    })
+
+    return sources
+}
+
 module.exports = {
     readCSV,
     readDirectory,
+    readSourcesNames,
 }
