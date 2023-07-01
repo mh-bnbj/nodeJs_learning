6070d95cdf47c2b921f2e5b1c95fc4d65cdd247c
mh-bnbj <www.mohamadhasan65@gmail.com>
2023-07-01 23:19:29 +0330

    e5_errorHandler02 add morgan to log req

--- s4_express-intro/error_handling/index.js
+++ s4_express-intro/error_handling/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const router = require('./route/index')
+const morgan = require('morgan')
 
 const app = express()
 const PORT = 3000
 
 app.use(express.static('public'))
+
+app.use(morgan('dev'))
+// app.use(morgan('tiny'))
+// app.use(morgan('combined'))
 
 app.use('/', router)
 
