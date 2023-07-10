efcd8f05e08afc873d682da2c8adcfecbfbcbe68
mh-bnbj <www.mohamadhasan65@gmail.com>
2023-07-10 19:55:00 +0330

    add search form

--- s6_authentication/newsWebsite/controllers/searchController.js
+++ s6_authentication/newsWebsite/controllers/searchController.js
@@ -1,25 +1,38 @@
+const { Op } = require('sequelize')
 const Category = require('../models/Category')
 const Post = require('../models/Post')
 
-const homepageController = async (req, res) => {
+const searchController = async (req, res) => {
     const categories = await Category.findAll()
     const activePageId = Number(req.query.page) || 1
     const offset = (Number(req.query.page) - 1) * 11 || 0
-    const counts = await Post.count()
+    const counts = await Post.count({
+        where: {
+            title: {
+                [Op.like]: `%${req.query.q}%`,
+            },
+        },
+    })
     const posts = await Post.findAll({
-        limit: 11,
+        where: {
+            title: {
+                [Op.like]: `%${req.query.q}%`,
+            },
+        },
+        limit: 10,
         offset,
         order: [['created_at', 'DESC']],
         include: Category,
     })
-    res.render('index', {
+    res.render('search', {
         categories: categories.map((category) => category.name),
         posts: posts,
         activeCategoryId: null,
         counts,
         activePageId,
         user: req.user,
+        query: req.query.q,
     })
 }
 
-module.exports = homepageController
+module.exports = searchController
--- s6_authentication/newsWebsite/routes/index.js
+++ s6_authentication/newsWebsite/routes/index.js
@@ -5,11 +5,13 @@ const postController = require('../controllers/postController')
 const aboutController = require('../controllers/aboutController')
 const contactController = require('../controllers/contactController')
 const categoryController = require('../controllers/categoryController')
+const searchController = require('../controllers/searchController')
 
 router.get('/', homepageController)
 router.get('/post/:id', postController)
 router.get('/category/:id', categoryController)
 router.get('/about', aboutController)
 router.get('/contact', contactController)
+router.get('/search', searchController)
 
 module.exports = router
